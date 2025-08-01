df358cdcbf772be38f8b28b8e3954c8cca77eee3
Evan Gehler <evangehler@gmail.com>
2025-08-01 17:27:21 -0400

    AlbumPage.jsx tweak

--- frontend/src/AlbumPage.jsx
+++ frontend/src/AlbumPage.jsx
@@ -1,7 +1,7 @@
 // AlbumPage.jsx
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Container, Group, Stack, Text, Card, Button } from '@mantine/core';
+import { Container, Group, Stack, Text, Card, Button, Rating } from '@mantine/core';
 import ReviewCard from './ReviewCard.jsx';
 import RatingSubmission from './components/RatingSubmission.jsx';
 
@@ -62,12 +62,27 @@ export default function AlbumPage() {
                 }} 
               />
 
+
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
                 <Text c="white" style={{ marginBottom: '0px' }} size="xl" fw={700}>{album.title}</Text>
                 <Text c="gray.3" style={{ marginBottom: '0px' }} size="md" fw={500}>{album.artist} ({album.year})</Text>
+              </div>
+              
               <Card className="glass-card" bg="#4c5897" style={{ textAlign: 'center' }}>
+                <div style ={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1rem' }}>
+                <Rating                          
+                  value={album.average_score?.toFixed(1)}
+                  fractions={2}
+                  readOnly
+                  size="lg"
+                  color="yellow"
+                 />
+                </div>
+               
                 <Text weight={600} color="white" size="lg">
                   Average Score: {album.average_score?.toFixed(1) || 'N/A'}
                 </Text>
+
               </Card>
             </Stack>
           </div>
