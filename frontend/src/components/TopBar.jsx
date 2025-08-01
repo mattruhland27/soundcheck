b32bc5a4546c30ebd49e1e6de142f5647d2b9e3d
Matthew Ruhland <ruhmatt1973@gmail.com>
2025-08-01 12:19:48 -0500

    Api protection fix

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -14,23 +14,30 @@ export default function TopBar({ username, setUsername }) {
   const handleLogout = () => {
     localStorage.removeItem("Authenticated");
     localStorage.removeItem("username");
+    localStorage.removeItem("token");
     setUsername(null);
     window.location.href = "/login";
   };
 
   return (
     <div className="glass-card-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem', background: '#262e4a' }}>
+        <div style={{gap:'1rem', display:'flex', justifyContent:'center', alignItems: 'center'}}>
       <Link to="/" style={{ textDecoration: 'none' }}>
         <span style={{ display: 'inline-block', width: 100 }}>
           <Image src={logo} alt="Logo" fit="contain" />
         </span>
       </Link>
+            {username && (
+        <input placeholder="Search" className={"input-search"} style={{display: 'flex', marginLeft:"20px"}} />
+                )}
+            </div>
 
       <Group>
+
         {username ? (
         // Show logout and username if logged in
           <>
-            <span style={{ color: 'white' }}>Logged in as <strong>{username}</strong></span>
+            <span style={{ color: 'white',fontWeight:'bold'}}>Logged in as <strong>{username}</strong></span>
             <Button variant="outline" onClick={handleLogout}>Logout</Button>
               {username == "admin" && (
               <Button variant={"filled"} onClick={()=> navigate('/users')}>Users</Button>)}
